4ba6e4ac99eb321e0e988f156e4fd3708e868f6c
Jaskaran singh <90018068+Jaskaran100k@users.noreply.github.com>
2024-05-12 02:09:05 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const TelegramBot = require('node-telegram-bot-api');
 const { exec } = require('child_process');
 
 // Ganti dengan token bot Telegram Anda
-const token = '7069670027:AAGrM4SX8wAyoFmZpKQQo83qS3aANewmleI';
+const token = '6842690571:AAFEPFadO2PpQNQqty0df0eeQKvRvHExSME';
 
 // Inisialisasi bot dengan token
 const bot = new TelegramBot(token, { polling: true });
